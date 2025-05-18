e2c80ae3819a8f1148a75f0479f50acfdeec5ee5
marciogameiro <marciogameiro@gmail.com>
2025-05-18 01:31:50 -0400

    Update build backend

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 # installer script
 rm -rf build
 rm -rf dist
-git submodule update --init --recursive
-#pip install . --ignore-installed --no-cache-dir
 pip install . --upgrade --no-deps --force-reinstall --no-cache-dir --user
